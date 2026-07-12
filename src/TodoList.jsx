dcf02651a51e59bc105216b11f74d1eeb7e3f1e6
AnnnAPr <programming.aap@gmail.com>
2026-07-12 11:34:44 -1000

    conditional rendering

--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -4,6 +4,8 @@ function TodoList({todoList}) {
 
   return (
     <>
+    {
+      todoList.length === 0 ? <p>Add todo above to get started</p> :
       <ul>
         {
           todoList.map((todo) => (
@@ -11,6 +13,14 @@ function TodoList({todoList}) {
           ))
         }
       </ul>
+    }
+      {/* <ul>
+        {
+          todoList.map((todo) => (
+            <TodoListItem key={todo.id} todo={todo} />
+          ))
+        }
+      </ul> */}
     </>
   );
 }
