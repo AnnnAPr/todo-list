0f332d923866bc80db76b141a1ab066525c487f6
AnnnAPr <programming.aap@gmail.com>
2026-09-03 08:35:45 -1000

    Add more description to About page

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -7,6 +7,11 @@ function AboutPage() {
         track, and complete your daily to-dos. Create tasks, mark them as done,
         and stay focused on what matters most.
       </p>
+      <section>
+        <p>
+          This application is a modern Single Page Application (SPA) built using React and powered by Vite for an ultra-fast development and build experience. Seamless client-side navigation is handled by React Router, allowing users to transition between views smoothly without full page reloads.
+        </p>
+      </section>
     </div>
   );
 }
